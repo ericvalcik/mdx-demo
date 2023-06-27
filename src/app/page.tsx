48181c1295d6cc3bc0bd402279f9e7c93def55d5
eric valcik <eric.valcik@gmail.com>
2023-06-27 18:43:22 +0200

    add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
+import "./global.css";
 import Counter from "@/components/Counter";
-import { readdirSync } from "fs";
-import { join } from "path";
-
-const getFiles = () => {
-  const files = readdirSync(join(process.cwd(), "src", "blog"));
-  return files.map((file) => file.replace(/\.mdx/, ""));
-};
 
 export default function BlogPost() {
-  const files = getFiles();
   return (
-    <div className="flex flex-col">
-      {files.map((slug, key) => (
-        <div key={key}>{slug}</div>
-      ))}
-      <Counter />
+    <div className="flex flex-col gap-4 p-10">
+      <div>
+        <Counter />
+      </div>
+      <div>
+        Try to go to{" "}
+        <a href={`/blog/post-001`} className="underline">
+          /blog/post-001
+        </a>
+        , and check the code in the repo.
+      </div>
     </div>
   );
 }
