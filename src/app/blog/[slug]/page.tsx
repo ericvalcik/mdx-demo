a204b65a2a3a99aa7530aa7452ddfc66d094c0ac
eric valcik <eric.valcik@gmail.com>
2023-06-27 18:15:01 +0200

    working mdx with app folder

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import { readdirSync } from "fs";
+import { join } from "path";
+import dynamic from 'next/dynamic';
+
+type BlogPostProps = {
+  params: {
+    slug: string;
+  }
+}
+
+export default async function BlogPost({ params: { slug } }: BlogPostProps) {
+  const Component = await dynamic(() => import((`@/blog/${slug}.mdx`)));
+  return <Component />
+}
+
+const getFiles = () => {
+  const files = readdirSync(join(process.cwd(), "src", "app", "blog"));
+  return files.map((file) => file.replace(/\.mdx/, ""));
+}
+
+export async function generateStaticParams() {
+  const files = getFiles();
+  return files.map((slug) => ({ slug }));
+}
